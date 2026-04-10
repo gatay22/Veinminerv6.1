16fd7d30475192e39bec63b189818f066034e419
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 16:59:54 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -4,16 +4,16 @@ using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 
-namespace MeleeProjectileRework
+namespace RangedTwinsStandard
 {
     [ApiVersion(2, 1)]
-    public class MeleePlugin : TerrariaPlugin
+    public class RangedPlugin : TerrariaPlugin
     {
-        public override string Name => "Unique Melee Projectiles";
+        public override string Name => "Twins Laser Ranged Dynamic";
         public override string Author => "Player";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(1, 2, 1);
 
-        public MeleePlugin(Main game) : base(game) { }
+        public RangedPlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
@@ -30,46 +30,40 @@ namespace MeleeProjectileRework
             Player p = tsPlayer.TPlayer;
             Item item = p.inventory[p.selectedItem];
 
-            // Cek apakah itu senjata Melee (dan bukan tools seperti kapak/pickaxe)
-            if (item != null && item.damage > 0 && item.melee && item.pick == 0 && item.axe == 0)
+            // Cek jika senjata Ranged
+            if (item != null && item.damage > 0 && item.ranged)
             {
-                // Trigger hanya saat animasi ayunan pedang dimulai
+                // Trigger saat senjata ditembakkan
                 if (p.itemAnimation == p.itemAnimationMax - 1 && p.itemAnimation > 0)
                 {
-                    ShootUniqueProjectile(tsPlayer, item);
+                    ApplyTwinsLaser(tsPlayer, item);
                 }
             }
         }
 
-        private void ShootUniqueProjectile(TSPlayer tsPlayer, Item item)
+        private void ApplyTwinsLaser(TSPlayer tsPlayer, Item item)
         {
+            // Peluang 20% (1 dari 5 tembakan)
+            if (Main.rand.Next(5) != 0) return;
+
             Vector2 pos = tsPlayer.TPlayer.Center;
-            Vector2 velocity = new Vector2(tsPlayer.TPlayer.direction * 12f, 0f);
-            int projID;
+            Vector2 targetPos = new Vector2(tsPlayer.TPlayer.lastMouseX + Main.screenPosition.X, tsPlayer.TPlayer.lastMouseY + Main.screenPosition.Y);
+            Vector2 baseVelocity = Vector2.Normalize(targetPos - pos) * 16f;
 
-            // LOGIKA PEMBEDA PROYEKTIL BERDASARKAN NAMA PEDANG
-            string name = item.Name ?? "";
+            int laserProjID = 83; // Laser Merah Twins (Retinazer)
 
-            if (name.Contains("Star")) 
-                projID = 12; // Falling Star
-            else if (name.Contains("Fire") || name.Contains("Fiery") || name.Contains("Flame")) 
-                projID = 188; // Magma Ball
-            else if (name.Contains("Ice") || name.Contains("Frost") || name.Contains("Frozen")) 
-                projID = 118; // Frost Beam
-            else if (name.Contains("Grass") || name.Contains("Blade of Grass") || name.Contains("Thorn")) 
-                projID = 45; // Spore Cloud
-            else if (name.Contains("Excalibur") || name.Contains("Light")) 
-                projID = 156; // Hallowed Beam (Laser Kuning)
-            else if (name.Contains("Night") || name.Contains("Dark")) 
-                projID = 274; // Shadow Beam
-            else 
-                projID = 157; // Laser standar (Purple Beam) untuk pedang lainnya
+            // Tembakkan 5 laser sekaligus
+            for (int i = 0; i < 5; i++)
+            {
+                // Spread tipis agar laser menyebar seperti kipas
+                float spread = MathHelper.ToRadians(-8f + (i * 4f)); 
+                Vector2 shotVelocity = baseVelocity.RotatedBy(spread);
 
-            // Tembakkan proyektil dengan damage sesuai senjata
-            int proj = Projectile.NewProjectile(null, pos, velocity, projID, item.damage, 5f, tsPlayer.Index);
-            
-            // Kirim data ke semua player agar visualnya muncul
-            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, proj);
+                // DAMAGE MENGIKUTI SENJATA (item.damage)
+                int proj = Projectile.NewProjectile(null, pos, shotVelocity, laserProjID, item.damage, 4f, tsPlayer.Index);
+                
+                NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, proj);
+            }
         }
 
         protected override void Dispose(bool disposing)
