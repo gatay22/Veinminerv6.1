5bfb0dc5956743936f5ce02f61f6a9ea02b064df
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 15:19:59 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -2,92 +2,65 @@ using System;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
-using Microsoft.Xna.Framework;
 
-namespace WhipRework
+namespace AccessoryOverdrive
 {
     [ApiVersion(2, 1)]
-    public class DynamicWhipPlugin : TerrariaPlugin
+    public class ReforgePlugin : TerrariaPlugin
     {
-        public override string Name => "Homing Whip Rework Fixed";
+        public override string Name => "Accessory 10 Percent Boost";
         public override string Author => "Player";
-        public override Version Version => new Version(1, 2, 2);
+        public override Version Version => new Version(1, 0, 0);
 
-        public DynamicWhipPlugin(Main game) : base(game) { }
+        public ReforgePlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
-            ServerApi.Hooks.NetGetData.Register(this, OnGetData);
+            ServerApi.Hooks.GameUpdate.Register(this, OnUpdate);
         }
 
-        private void OnGetData(GetDataEventArgs args)
+        private void OnUpdate(EventArgs args)
         {
-            if (args.MsgID != PacketTypes.PlayerUpdate) return;
-
-            TSPlayer tsPlayer = TShock.Players[args.Msg.whoAmI];
-            if (tsPlayer == null || !tsPlayer.Active || tsPlayer.TPlayer == null) return;
-
-            Player p = tsPlayer.TPlayer;
-            Item item = p.inventory[p.selectedItem];
-
-            if (item != null && item.damage > 0 && item.summon && item.pick == 0)
+            for (int i = 0; i < TShock.Players.Length; i++)
             {
-                if (p.itemAnimation == p.itemAnimationMax - 1 && p.itemAnimation > 0)
+                TSPlayer tsPlayer = TShock.Players[i];
+                if (tsPlayer == null || !tsPlayer.Active || tsPlayer.TPlayer == null) continue;
+
+                Player p = tsPlayer.TPlayer;
+
+                // Slot 3 sampai 9 adalah slot KHUSUS AKSESORIS di Terraria
+                // (Slot 0-2 itu Armor, jadi kita lewati)
+                for (int j = 3; j < 10; j++)
                 {
-                    ApplyHomingWhip(tsPlayer, item);
-                }
-            }
-        }
+                    Item item = p.armor[j];
+                    if (item == null || item.type == 0 || item.prefix == 0) continue;
 
-        private void ApplyHomingWhip(TSPlayer tsPlayer, Item item)
-        {
-            Vector2 pos = tsPlayer.TPlayer.Center;
-            int projID = 307; // Default: Chlorophyte Orb
-            
-            string itemName = item.Name ?? "";
-
-            if (itemName.Contains("Snapthorn")) projID = 181;
-            else if (itemName.Contains("Fire") || itemName.Contains("Spicer")) projID = 504;
-            else if (itemName.Contains("Cool") || itemName.Contains("Ice")) projID = 344;
-            else if (itemName.Contains("Kaleidoscope")) projID = 636;
-            else if (itemName.Contains("Morning Star")) projID = 729;
-
-            Vector2 velocity = new Vector2(tsPlayer.TPlayer.direction * 10f, 0f);
-            float closestDist = 600f; 
-            int targetNPC = -1;
-
-            for (int i = 0; i < Main.maxNPCs; i++)
-            {
-                NPC npc = Main.npc[i];
-                if (npc != null && npc.active && !npc.friendly && npc.damage > 0)
-                {
-                    float dist = Vector2.Distance(pos, npc.Center);
-                    if (dist < closestDist)
+                    // Logika Tambahan 6% (Biar total jadi 10%)
+                    switch (item.prefix)
                     {
-                        closestDist = dist;
-                        targetNPC = i;
+                        case 65: // Menacing (+4% Damage)
+                            p.GetDamage(DamageClass.Generic) += 0.06f;
+                            break;
+                        case 68: // Lucky (+4% Crit)
+                            p.GetCritChance(DamageClass.Generic) += 6f;
+                            break;
+                        case 62: // Warding (+4 Defense)
+                            p.statDefense += 6; // Bonus flat defense
+                            break;
+                        case 67: // Quick (+4% Move Speed)
+                            p.moveSpeed += 0.06f;
+                            break;
+                        case 66: // Violent (+4% Melee Speed)
+                            p.GetAttackSpeed(DamageClass.Melee) += 0.06f;
+                            break;
                     }
                 }
             }
-
-            if (targetNPC != -1)
-            {
-                velocity = Vector2.Normalize(Main.npc[targetNPC].Center - pos) * 12f;
-            }
-
-            int proj = Projectile.NewProjectile(null, pos, velocity, projID, item.damage, 5f, tsPlayer.Index);
-            
-            if (targetNPC != -1 && proj >= 0 && proj < Main.maxProjectiles) 
-            {
-                Main.projectile[proj].ai[0] = targetNPC;
-            }
-
-            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, proj);
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing) ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
+            if (disposing) ServerApi.Hooks.GameUpdate.Deregister(this, OnUpdate);
             base.Dispose(disposing);
         }
     }
