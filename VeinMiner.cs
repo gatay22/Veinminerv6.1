08c0d65c1914cf8fb6625e495a26dde6f5b31385
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 15:09:35 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -1,19 +1,20 @@
 using System;
+using System.Linq;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 
-namespace MeleeRework
+namespace WhipRework
 {
     [ApiVersion(2, 1)]
-    public class MeleeReworkPlugin : TerrariaPlugin
+    public class DynamicWhipPlugin : TerrariaPlugin
     {
-        public override string Name => "Ultimate Melee Rework";
+        public override string Name => "Homing Whip Rework";
         public override string Author => "Player";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(1, 2, 0);
 
-        public MeleeReworkPlugin(Main game) : base(game) { }
+        public DynamicWhipPlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
@@ -24,43 +25,66 @@ namespace MeleeRework
         {
             if (args.MsgID != PacketTypes.PlayerUpdate) return;
 
-            // PERBAIKAN: Cara ambil player di TShock 6.1
             TSPlayer tsPlayer = TShock.Players[args.Msg.whoAmI];
             if (tsPlayer == null || !tsPlayer.Active || tsPlayer.TPlayer == null) return;
 
             Player p = tsPlayer.TPlayer;
             Item item = p.inventory[p.selectedItem];
 
-            // PERBAIKAN: Cek Melee secara manual (paling aman buat build)
-            if (item != null && item.damage > 0 && item.melee && item.pick == 0 && item.axe == 0)
+            if (item != null && item.damage > 0 && item.summon)
             {
                 if (p.itemAnimation == p.itemAnimationMax - 1 && p.itemAnimation > 0)
                 {
-                    ApplyMeleeGodEffects(tsPlayer, item);
+                    ApplyHomingWhip(tsPlayer, item);
                 }
             }
         }
 
-        private void ApplyMeleeGodEffects(TSPlayer tsPlayer, Item item)
+        private void ApplyHomingWhip(TSPlayer tsPlayer, Item item)
         {
             Vector2 pos = tsPlayer.TPlayer.Center;
-            
-            // PERBAIKAN: Arah serangan berdasarkan arah hadap player (karena LastNetConfig error)
-            float speed = 14f;
-            Vector2 velocity = new Vector2(tsPlayer.TPlayer.direction * speed, 0f); 
-            
-            // Jika player mengayun ke atas/bawah sedikit
-            if (tsPlayer.TPlayer.gravDir == -1f) velocity.Y = -speed;
+            int projID;
 
-            // 1. Proyektil Blue Flare (ID: 729)
-            int p1 = Projectile.NewProjectile(null, pos, velocity, 729, item.damage * 2, 6f, tsPlayer.Index);
-            
-            // 2. Proyektil Solar Flare (ID: 608) untuk efek "Spark" di badan
-            int p2 = Projectile.NewProjectile(null, pos, Vector2.Zero, 608, item.damage, 4f, tsPlayer.Index);
+            // Pilih ID proyektil berdasarkan nama cambuk
+            if (item.name.Contains("Snapthorn")) projID = 181; // Bee (Homing otomatis)
+            else if (item.name.Contains("Fire") || item.name.Contains("Spicer")) projID = 504; // Daybreak/Solar Flare
+            else if (item.name.Contains("Cool") || item.name.Contains("Ice")) projID = 344; // Frost Shard
+            else if (item.name.Contains("Kaleidoscope")) projID = 636; // Rainbow Crystal
+            else projID = 307; // Chlorophyte Orb (Homing sangat kuat)
 
-            // Kirim data ke semua player
-            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, p1);
-            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, p2);
+            // LOGIKA MENCARI MUSUH TERDEKAT (Homing)
+            Vector2 velocity = new Vector2(tsPlayer.TPlayer.direction * 10f, 0f);
+            float closestDist = 500f; // Jarak deteksi musuh (pixel)
+            int targetNPC = -1;
+
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC npc = Main.npc[i];
+                if (npc.active && !npc.friendly && npc.damage > 0)
+                {
+                    float dist = Vector2.Distance(pos, npc.Center);
+                    if (dist < closestDist)
+                    {
+                        closestDist = dist;
+                        targetNPC = i;
+                    }
+                }
+            }
+
+            // Jika musuh ketemu, arahkan velocity ke musuh
+            if (targetNPC != -1)
+            {
+                velocity = Vector2.Normalize(Main.npc[targetNPC].Center - pos) * 12f;
+            }
+
+            int proj = Projectile.NewProjectile(null, pos, velocity, projID, item.damage, 5f, tsPlayer.Index);
+            
+            // Beritahu proyektil siapa targetnya (untuk proyektil tertentu)
+            if (targetNPC != -1) {
+                Main.projectile[proj].ai[0] = targetNPC;
+            }
+
+            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, proj);
         }
 
         protected override void Dispose(bool disposing)
