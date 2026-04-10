041b11aed9eb5e7e7fbe6ec9088229b9ed77c623
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 14:25:29 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -1,67 +1,83 @@
+using System;
+using System.IO;
+using System.Linq;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 
-[ApiVersion(2, 1)]
-public class VeinMiner : TerrariaPlugin
+namespace MyVeinMiner
 {
-    public override string Name => "Simple Vein Miner";
-    public override string Author => "Player";
-    public override Version Version => new Version(1, 0, 0);
-
-    public VeinMiner(Main game) : base(game) { }
-
-    public override void Initialize()
+    [ApiVersion(2, 1)]
+    public class VeinMiner : TerrariaPlugin
     {
-        ServerApi.Hooks.NetGetData.Register(this, OnGetData);
-    }
+        public override string Name => "Simple Vein Miner";
+        public override string Author => "Player";
+        public override Version Version => new Version(1, 0, 0);
 
-    private void OnGetData(GetDataEventArgs args)
-    {
-        if (args.MsgID != PacketTypes.TileManipulation) return;
+        public VeinMiner(Main game) : base(game) { }
 
-        using (var reader = new BinaryReader(new MemoryStream(args.Msg.readBuffer, args.Index, args.Length)))
+        public override void Initialize()
         {
-            byte action = reader.ReadByte();
-            short x = reader.ReadInt16();
-            short y = reader.ReadInt16();
-            ushort type = reader.ReadUInt16();
-
-            if (action == 0 && IsOre(Main.tile[x, y].type))
-            {
-                MineVein(x, y, Main.tile[x, y].type);
-            }
+            ServerApi.Hooks.NetGetData.Register(this, OnGetData);
         }
-    }
 
-    private bool IsOre(int type)
-    {
-        // Daftar ID Bijih (Copper, Iron, Gold, dll)
-        int[] ores = { 7, 8, 9, 22, 25, 37, 48, 56, 107, 108, 111, 121, 166, 167, 168, 169 };
-        return ores.Contains(type);
-    }
-
-    private void MineVein(int x, int y, int type)
-    {
-        // Loop simpel untuk hancurin blok sekitar
-        for (int i = x - 1; i <= x + 1; i++)
+        private void OnGetData(GetDataEventArgs args)
         {
-            for (int j = y - 1; j <= y + 1; j++)
+            // PERBAIKAN: Ganti TileManipulation jadi TileEdit
+            if (args.MsgID != PacketTypes.TileEdit) return;
+
+            using (var reader = new BinaryReader(new MemoryStream(args.Msg.readBuffer, args.Index, args.Length)))
             {
-                if (Main.tile[i, j].active() && Main.tile[i, j].type == type)
+                byte action = reader.ReadByte();
+                short x = reader.ReadInt16();
+                short y = reader.ReadInt16();
+                ushort type = reader.ReadUInt16();
+
+                if (action == 0) // Menghancurkan blok
                 {
-                    WorldGen.KillTile(i, j, false, false, false);
-                    TSPlayer.All.SendTileSquare(i, j, 1);
-                    // Catatan: Untuk skala besar butuh rekursi, tapi ini cukup untuk awal
+                    if (x < 0 || y < 0 || x >= Main.maxTilesX || y >= Main.maxTilesY) return;
+
+                    var tile = Main.tile[x, y];
+                    if (tile.active() && IsOre(tile.type))
+                    {
+                        MineVein(x, y, tile.type);
+                    }
                 }
             }
         }
-    }
 
-    protected override void Dispose(bool disposing)
-    {
-        if (disposing) ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
-        base.Dispose(disposing);
+        private bool IsOre(int type)
+        {
+            // Daftar ID bijih standar
+            int[] ores = { 7, 8, 9, 22, 25, 37, 48, 56, 107, 108, 111, 121, 166, 167, 168, 169 };
+            return ores.Contains(type);
+        }
+
+        private void MineVein(int x, int y, int type)
+        {
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if (i < 0 || j < 0 || i >= Main.maxTilesX || j >= Main.maxTilesY) continue;
+
+                    if (Main.tile[i, j].active() && Main.tile[i, j].type == type)
+                    {
+                        WorldGen.KillTile(i, j, false, false, false);
+                        
+                        // PERBAIKAN: Pakai SendTileSquare versi terbaru (tanpa TSPlayer.All)
+                        // Atau kirim ke semua player dengan TSPlayer.All.SendTileSquare
+                        TSPlayer.All.SendTileSquare(i, j); 
+                    }
+                }
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
+            base.Dispose(disposing);
+        }
     }
 }
