fedb1689e91e7c953c8f88c1ba9c34d163e4e841
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 19:54:38 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -11,7 +11,7 @@ namespace UltimateVisualPlugin
     {
         public override string Name => "Godly Armor & Accessory Effects";
         public override string Author => "Gemini";
-        public override Version Version => new Version(1, 1, 1);
+        public override Version Version => new Version(1, 1, 2);
 
         private int timer = 0;
 
@@ -33,12 +33,11 @@ namespace UltimateVisualPlugin
 
                 Player p = tsPlayer.TPlayer;
 
-                // --- BAGIAN 1: ARMOR SHIELD (DAMAGE TINGGI) ---
+                // --- 1. LOGIKA ARMOR SHIELD ---
                 int headID = p.armor[0].type;
                 int armProj = -1;
                 int armDmg = 10;
 
-                // Logika Armor (Sama seperti sebelumnya)
                 if (headID == 79 || headID == 80 || headID == 81) { armProj = 157; armDmg = 12; }
                 else if (headID == 231 || headID == 2763) { armProj = 15; armDmg = 40; }
                 else if (headID >= 2851 && headID <= 2862) { armProj = 614; armDmg = 70; }
@@ -46,11 +45,48 @@ namespace UltimateVisualPlugin
                 if (armProj != -1 && timer % 60 == 0)
                 {
                     int pID = Projectile.NewProjectile(null, p.Center, Vector2.Zero, armProj, armDmg, 5f, tsPlayer.Index);
-                    if (pID < 1000)
+                    if (pID >= 0 && pID < 1000)
                     {
                         Main.projectile[pID].timeLeft = 40;
                         NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, pID);
                     }
                 }
 
-                //
+                // --- 2. LOGIKA AKSESORIS ---
+                for (int i = 3; i <= 8; i++)
+                {
+                    Item acc = p.armor[i];
+                    if (acc == null || acc.type == 0) continue;
+
+                    // Efek Sayap
+                    if (acc.wingSlot > 0 && p.velocity.Y != 0 && timer % 30 == 0)
+                    {
+                        int wID = Projectile.NewProjectile(null, p.Bottom, Vector2.Zero, 502, 5, 0f, tsPlayer.Index);
+                        if (wID >= 0 && wID < 1000)
+                        {
+                            Main.projectile[wID].timeLeft = 20;
+                            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, wID);
+                        }
+                    }
+
+                    // Efek Sepatu
+                    if ((acc.type == 54 || acc.type == 1862 || acc.type == 5000) && Math.Abs(p.velocity.X) > 8)
+                    {
+                        int sID = Projectile.NewProjectile(null, p.Bottom, Vector2.Zero, 612, 8, 0f, tsPlayer.Index);
+                        if (sID >= 0 && sID < 1000)
+                        {
+                            Main.projectile[sID].timeLeft = 15;
+                            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, sID);
+                        }
+                    }
+                } // Tutup Loop Aksesoris
+            } // Tutup Foreach Player
+        } // Tutup OnUpdate
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) ServerApi.Hooks.GameUpdate.Deregister(this, OnUpdate);
+            base.Dispose(disposing);
+        }
+    }
+}
