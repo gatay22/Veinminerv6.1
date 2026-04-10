5008c212eff4d7c722647d593876676729edadec
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 14:46:05 +0700

    Rename VeinMiner to MeleeRework and update logic

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -1,21 +1,20 @@
 using System;
 using System.IO;
-using System.Linq;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 
-namespace MyVeinMiner
+namespace MeleeRework
 {
     [ApiVersion(2, 1)]
-    public class VeinMiner : TerrariaPlugin
+    public class MeleeReworkPlugin : TerrariaPlugin
     {
-        public override string Name => "Simple Vein Miner";
+        public override string Name => "Ultimate Melee Rework";
         public override string Author => "Player";
         public override Version Version => new Version(1, 0, 0);
 
-        public VeinMiner(Main game) : base(game) { }
+        public MeleeReworkPlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
@@ -24,53 +23,48 @@ namespace MyVeinMiner
 
         private void OnGetData(GetDataEventArgs args)
         {
-            // PERBAIKAN LAGI: Di TShock 6.1, namanya adalah PacketTypes.Tile
-            if (args.MsgID != PacketTypes.Tile) return;
+            if (args.MsgID != PacketTypes.PlayerUpdate) return;
 
-            using (var reader = new BinaryReader(new MemoryStream(args.Msg.readBuffer, args.Index, args.Length)))
+            TSPlayer tsPlayer = TShock.Players[args.Player.whoAmI];
+            if (tsPlayer == null || !tsPlayer.Active || tsPlayer.SelectedItem == null) return;
+
+            Player p = tsPlayer.TPlayer;
+            Item item = tsPlayer.SelectedItem;
+
+            // DETEKSI SEMUA MELEE: Pedang, Tombak, Yoyo, Flail, dll.
+            // Kita kecualikan alat kerja (Pickaxe/Axe) biar gak ganggu pas nambang.
+            if (item.damage > 0 && item.countsAsClass(DamageClass.Melee) && item.pick == 0 && item.axe == 0)
             {
-                byte action = reader.ReadByte();
-                short x = reader.ReadInt16();
-                short y = reader.ReadInt16();
-                ushort type = reader.ReadUInt16();
-
-                if (action == 0) // Menghancurkan blok
+                // Cek saat animasi ayunan dimulai
+                if (p.itemAnimation == p.itemAnimationMax - 1 && p.itemAnimation > 0)
                 {
-                    if (x < 0 || y < 0 || x >= Main.maxTilesX || y >= Main.maxTilesY) return;
-
-                    var tile = Main.tile[x, y];
-                    if (tile.active() && IsOre(tile.type))
-                    {
-                        MineVein(x, y, tile.type);
-                    }
+                    ApplyMeleeGodEffects(tsPlayer, item);
                 }
             }
         }
 
-        private bool IsOre(int type)
+        private void ApplyMeleeGodEffects(TSPlayer tsPlayer, Item item)
         {
-            int[] ores = { 7, 8, 9, 22, 25, 37, 48, 56, 107, 108, 111, 121, 166, 167, 168, 169 };
-            return ores.Contains(type);
-        }
+            Vector2 pos = tsPlayer.TPlayer.Center;
+            
+            // Hitung arah mouse player
+            float speed = 14f;
+            float angle = (float)Math.Atan2(tsPlayer.LastNetConfig.Y - pos.Y, tsPlayer.LastNetConfig.X - pos.X);
+            Vector2 velocity = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)) * speed;
 
-        private void MineVein(int x, int y, int type)
-        {
-            for (int i = x - 1; i <= x + 1; i++)
-            {
-                for (int j = y - 1; j <= y + 1; j++)
-                {
-                    if (i < 0 || j < 0 || i >= Main.maxTilesX || j >= Main.maxTilesY) continue;
+            // 1. PROYEKTIL UTAMA: Blue Flare (ID: 729 - Efeknya keren banget)
+            int p1 = Projectile.NewProjectile(null, pos, velocity, 729, item.damage * 2, 6f, tsPlayer.Index);
+            
+            // 2. PROYEKTIL TAMBAHAN: Solar Flare (ID: 608 - Efek api berputar)
+            // Biar mepet sama player, buat efek tebasan area
+            int p2 = Projectile.NewProjectile(null, pos, velocity * 0.5f, 608, item.damage, 4f, tsPlayer.Index);
 
-                    if (Main.tile[i, j].active() && Main.tile[i, j].type == type)
-                    {
-                        WorldGen.KillTile(i, j, false, false, false);
-                        
-                        // PERBAIKAN WARNING: Kita gunakan SendTileSquare standar
-                        // TShock 6.1 biasanya minta (x, y, size)
-                        TSPlayer.All.SendTileSquare(i, j, 1);
-                    }
-                }
-            }
+            // Kirim data ke semua player biar visualnya muncul
+            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, p1);
+            NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, p2);
+            
+            // Efek suara biar kerasa berat
+            tsPlayer.SendData(PacketTypes.PlaySound, "", 2, pos.X, pos.Y, 71); 
         }
 
         protected override void Dispose(bool disposing)
