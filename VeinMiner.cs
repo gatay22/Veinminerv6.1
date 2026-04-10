21665094e222706cb2493a6ee32c1cd4263021a8
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 17:01:47 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -4,14 +4,14 @@ using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 
-namespace RangedTwinsStandard
+namespace RangedTwinsFinal
 {
     [ApiVersion(2, 1)]
     public class RangedPlugin : TerrariaPlugin
     {
-        public override string Name => "Twins Laser Ranged Dynamic";
+        public override string Name => "Twins Laser Ranged Fixed";
         public override string Author => "Player";
-        public override Version Version => new Version(1, 2, 1);
+        public override Version Version => new Version(1, 2, 2);
 
         public RangedPlugin(Main game) : base(game) { }
 
@@ -30,10 +30,8 @@ namespace RangedTwinsStandard
             Player p = tsPlayer.TPlayer;
             Item item = p.inventory[p.selectedItem];
 
-            // Cek jika senjata Ranged
             if (item != null && item.damage > 0 && item.ranged)
             {
-                // Trigger saat senjata ditembakkan
                 if (p.itemAnimation == p.itemAnimationMax - 1 && p.itemAnimation > 0)
                 {
                     ApplyTwinsLaser(tsPlayer, item);
@@ -43,23 +41,23 @@ namespace RangedTwinsStandard
 
         private void ApplyTwinsLaser(TSPlayer tsPlayer, Item item)
         {
-            // Peluang 20% (1 dari 5 tembakan)
             if (Main.rand.Next(5) != 0) return;
 
             Vector2 pos = tsPlayer.TPlayer.Center;
-            Vector2 targetPos = new Vector2(tsPlayer.TPlayer.lastMouseX + Main.screenPosition.X, tsPlayer.TPlayer.lastMouseY + Main.screenPosition.Y);
-            Vector2 baseVelocity = Vector2.Normalize(targetPos - pos) * 16f;
+            
+            // Karena lastMouseX tidak ada di server, kita gunakan arah hadap player (direction)
+            // 16f adalah kecepatan laser
+            Vector2 baseVelocity = new Vector2(tsPlayer.TPlayer.direction * 16f, 0f);
 
-            int laserProjID = 83; // Laser Merah Twins (Retinazer)
+            int laserProjID = 83; // Laser Merah Twins
 
-            // Tembakkan 5 laser sekaligus
             for (int i = 0; i < 5; i++)
             {
-                // Spread tipis agar laser menyebar seperti kipas
-                float spread = MathHelper.ToRadians(-8f + (i * 4f)); 
+                // Variasi sudut supaya menyebar (kipas)
+                float spread = MathHelper.ToRadians(-10f + (i * 5f)); 
                 Vector2 shotVelocity = baseVelocity.RotatedBy(spread);
 
-                // DAMAGE MENGIKUTI SENJATA (item.damage)
+                // Damage murni mengikuti senjata
                 int proj = Projectile.NewProjectile(null, pos, shotVelocity, laserProjID, item.damage, 4f, tsPlayer.Index);
                 
                 NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, proj);
