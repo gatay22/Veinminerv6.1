0641ffb6273d93d287ddbde28e768068301caeb9
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 19:43:53 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -1,121 +1,93 @@
 using System;
-using System.IO;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 
-namespace PersonalizedRangedArsenal
+namespace AdaptiveArmorOnly
 {
     [ApiVersion(2, 1)]
-    public class RangedPlugin : TerrariaPlugin
+    public class ArmorPlugin : TerrariaPlugin
     {
-        public override string Name => "Personalized Ranged Elements";
+        public override string Name => "Adaptive Armor Shields";
         public override string Author => "Gemini";
-        public override Version Version => new Version(2, 6, 0);
+        public override Version Version => new Version(1, 0, 0);
 
-        public RangedPlugin(Main game) : base(game) { }
+        private int timer = 0;
+
+        public ArmorPlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
-            ServerApi.Hooks.NetGetData.Register(this, OnGetData);
+            ServerApi.Hooks.GameUpdate.Register(this, OnUpdate);
         }
 
-        private void OnGetData(GetDataEventArgs args)
+        private void OnUpdate(EventArgs args)
         {
-            if (args.MsgID != PacketTypes.PlayerUpdate) return;
+            timer++;
+            // Update setiap 15 tick agar performa server tetap enteng
+            if (timer % 15 != 0) return;
 
-            TSPlayer tsPlayer = TShock.Players[args.Msg.whoAmI];
-            if (tsPlayer == null || !tsPlayer.Active || tsPlayer.TPlayer == null) return;
-
-            Player p = tsPlayer.TPlayer;
-            Item item = p.inventory[p.selectedItem];
-
-            // Cek jika item adalah senjata Ranged dengan damage
-            if (item != null && item.damage > 0 && item.ranged)
+            foreach (TSPlayer tsPlayer in TShock.Players)
             {
-                // Trigger saat senjata ditembakkan (frame awal animasi)
-                if (p.itemAnimation == p.itemAnimationMax - 1 && p.itemAnimation > 0)
+                if (tsPlayer == null || !tsPlayer.Active || tsPlayer.TPlayer == null || tsPlayer.Dead) continue;
+
+                Player p = tsPlayer.TPlayer;
+                int headID = p.armor[0].type; // Mendeteksi helm/topi armor
+
+                int projID = -1;
+                int damage = 10;
+                float kb = 3f;
+
+                // --- DAFTAR KOMPLIT EFEK ARMOR ---
+
+                // 1. ORE AWAL (Copper, Iron, Lead, Tin, Wood)
+                if (headID == 79 || headID == 80 || headID == 81 || headID == 76 || headID == 77 || headID == 78) 
                 {
-                    ApplyUniqueRangedEffect(tsPlayer, item);
+                    projID = 157; // Metal Shard (Piringan Besi)
+                    damage = 12;
                 }
-            }
-        }
-
-        private void ApplyUniqueRangedEffect(TSPlayer tsPlayer, Item item)
-        {
-            Vector2 pos = tsPlayer.TPlayer.Center;
-            int dir = tsPlayer.TPlayer.direction;
-            Vector2 vel = new Vector2(dir * 15f, 0f); // Kecepatan peluru tambahan
-            int id = item.type;
-            Random rand = new Random();
-
-            int projID = -1;
-            int count = 1;
-            float dmgMult = 1f;
-
-            // 1. ELEMEN API (Molten Fury, Hellwing Bow, Phoenix Blaster)
-            if (id == 120 || id == 3247 || id == 197)
-            {
-                projID = 15; // Fireball
-                count = 2;
-            }
-            // 2. ELEMEN ES (Ice Bow, Snowman Cannon)
-            else if (id == 725 || id == 1946)
-            {
-                projID = 118; // Frost Beam
-            }
-            // 3. ELEMEN LISTRIK/HI-TECH (Megashark, Laser Rifle, Gatligator)
-            else if (id == 533 || id == 434 || id == 1255)
-            {
-                projID = 440; // Laser biru elektrik
-                count = 2;
-            }
-            // 4. ELEMEN DARK/CORRUPTION (Onyx Blaster, Dart Rifle)
-            else if (id == 3788 || id == 3019)
-            {
-                projID = 496; // Shadowflame
-            }
-            // 5. ELEMEN NATURE/POISON (Chlorophyte Shotbow, Bee's Knees)
-            else if (id == 1229 || id == 2888)
-            {
-                projID = 226; // Chlorophyte Seeker (Ngejar musuh)
-            }
-            // 6. ELEMEN COSMIC/ENDGAME (S.D.M.G, Phantasm, Vortex Beater)
-            else if (id == 1553 || id == 3475 || id == 3476)
-            {
-                projID = 614; // Nebula Sphere
-                count = 3;
-                dmgMult = 1.2f;
-            }
-            // 7. ELEMEN DARAH/CRIMSON (Dart Pistol, Tendon Bow)
-            else if (id == 3020 || id == 796)
-            {
-                projID = 305; // Ichor Splash
-            }
-
-            // Eksekusi penembakan proyektil elemen
-            if (projID != -1)
-            {
-                for (int i = 0; i < count; i++)
+                // 2. ORE MEWAH (Gold, Platinum, Silver, Tungsten)
+                else if (headID == 82 || headID == 83 || headID == 414 || headID == 415)
                 {
-                    float spread = MathHelper.ToRadians(rand.Next(-8, 9));
-                    int pID = Projectile.NewProjectile(null, pos, vel.RotatedBy(spread), projID, (int)(item.damage * dmgMult), 3f, tsPlayer.Index);
-                    NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, pID);
+                    projID = 156; // Gold Beam (Kilatan Emas)
+                    damage = 18;
                 }
-            }
-            // DEFAULT: Efek laser Twins standar untuk senjata lainnya
-            else
-            {
-                int pID = Projectile.NewProjectile(null, pos, vel, 83, (int)(item.damage * 0.7), 2f, tsPlayer.Index);
-                NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, pID);
-            }
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing) ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
-            base.Dispose(disposing);
-        }
-    }
-}
+                // 3. ARMOR API (Molten, Solar Flare)
+                else if (headID == 231 || headID == 2763)
+                {
+                    projID = 15; // Fireball (Bola Api)
+                    damage = 35;
+                    kb = 6f;
+                }
+                // 4. ARMOR GELAP (Shadow, Ancient Shadow)
+                else if (headID == 101 || headID == 102)
+                {
+                    projID = 496; // Shadowflame (Api Ungu)
+                    damage = 25;
+                }
+                // 5. ARMOR DARAH (Crimson)
+                else if (headID == 792)
+                {
+                    projID = 305; // Ichor Splash (Cipratan Darah)
+                    damage = 25;
+                }
+                // 6. ARMOR HUTAN (Jungle, Chlorophyte)
+                else if (headID == 228 || headID == 1001 || headID == 1002 || headID == 1003)
+                {
+                    projID = 228; // Spore Cloud (Awan Racun)
+                    damage = 20;
+                }
+                // 7. ARMOR LEBAH (Bee Armor)
+                else if (headID == 2361)
+                {
+                    projID = 181; // Bees (Lebah Penjaga)
+                    damage = 15;
+                }
+                // 8. ARMOR ES (Frost Armor)
+                else if (headID == 684)
+                {
+                    projID = 118; // Frost Beam (Laser Es)
+                    damage = 28;
+                }
+                // 9. ARMOR SUCI (Hallowed Armor)
