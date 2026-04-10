96f2b43f4d704bbd5128af4d25a33c728a4fa543
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 19:36:33 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -5,16 +5,16 @@ using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 
-namespace UniqueMeleeMaster
+namespace PersonalizedRangedArsenal
 {
     [ApiVersion(2, 1)]
-    public class MeleePlugin : TerrariaPlugin
+    public class RangedPlugin : TerrariaPlugin
     {
-        public override string Name => "Personalized Melee Arsenal";
+        public override string Name => "Personalized Ranged Elements";
         public override string Author => "Gemini";
-        public override Version Version => new Version(2, 4, 0);
+        public override Version Version => new Version(2, 6, 0);
 
-        public MeleePlugin(Main game) : base(game) { }
+        public RangedPlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
@@ -31,64 +31,83 @@ namespace UniqueMeleeMaster
             Player p = tsPlayer.TPlayer;
             Item item = p.inventory[p.selectedItem];
 
-            if (item != null && item.damage > 0 && item.melee)
+            // Cek jika item adalah senjata Ranged dengan damage
+            if (item != null && item.damage > 0 && item.ranged)
             {
+                // Trigger saat senjata ditembakkan (frame awal animasi)
                 if (p.itemAnimation == p.itemAnimationMax - 1 && p.itemAnimation > 0)
                 {
-                    ApplyUniqueMeleeEffect(tsPlayer, item);
+                    ApplyUniqueRangedEffect(tsPlayer, item);
                 }
             }
         }
 
-        private void ApplyUniqueMeleeEffect(TSPlayer tsPlayer, Item item)
+        private void ApplyUniqueRangedEffect(TSPlayer tsPlayer, Item item)
         {
             Vector2 pos = tsPlayer.TPlayer.Center;
             int dir = tsPlayer.TPlayer.direction;
-            Vector2 vel = new Vector2(dir * 14f, 0f);
+            Vector2 vel = new Vector2(dir * 15f, 0f); // Kecepatan peluru tambahan
             int id = item.type;
+            Random rand = new Random();
 
             int projID = -1;
-            float damageMult = 1f;
+            int count = 1;
+            float dmgMult = 1f;
 
-            // 1. KATEGORI API (Volcano, Molten, Solar)
-            if (id == 121 || id == 3473 || id == 112) projID = 15; 
-            
-            // 2. KATEGORI ES (Ice Blade, Frostbrand)
-            else if (id == 724 || id == 674 || id == 675) projID = 118;
-
-            // 3. KATEGORI SHADOW/DARK (Night's Edge, Breaker Blade)
-            else if (id == 273 || id == 46) projID = 496; // Shadowflame burst
-
-            // 4. KATEGORI BLOOD/CRIMSON (Blood Butcherer, Blademaster)
-            else if (id == 795 || id == 3211) projID = 305; // Ichor splash
-
-            // 5. KATEGORI HALLOWED/LIGHT (Excalibur, Gungnir)
-            else if (id == 368 || id == 550) projID = 156; // Beam Sword ray
-
-            // 6. KATEGORI NATURE (Blade of Grass, Muramasa)
-            else if (id == 190 || id == 155) projID = 228; // Spore cloud
-
-            // 7. KATEGORI COSMIC/ENDGAME (Terra Blade, Influx Waver, Star Wrath)
-            else if (id == 757 || id == 2880 || id == 3063) 
+            // 1. ELEMEN API (Molten Fury, Hellwing Bow, Phoenix Blaster)
+            if (id == 120 || id == 3247 || id == 197)
             {
-                projID = 614; // Nebula sphere
-                damageMult = 1.2f;
+                projID = 15; // Fireball
+                count = 2;
             }
-            
-            // 8. KATEGORI KHUSUS ZENITH (Kalau ada yang punya)
-            else if (id == 4956) projID = 706;
+            // 2. ELEMEN ES (Ice Bow, Snowman Cannon)
+            else if (id == 725 || id == 1946)
+            {
+                projID = 118; // Frost Beam
+            }
+            // 3. ELEMEN LISTRIK/HI-TECH (Megashark, Laser Rifle, Gatligator)
+            else if (id == 533 || id == 434 || id == 1255)
+            {
+                projID = 440; // Laser biru elektrik
+                count = 2;
+            }
+            // 4. ELEMEN DARK/CORRUPTION (Onyx Blaster, Dart Rifle)
+            else if (id == 3788 || id == 3019)
+            {
+                projID = 496; // Shadowflame
+            }
+            // 5. ELEMEN NATURE/POISON (Chlorophyte Shotbow, Bee's Knees)
+            else if (id == 1229 || id == 2888)
+            {
+                projID = 226; // Chlorophyte Seeker (Ngejar musuh)
+            }
+            // 6. ELEMEN COSMIC/ENDGAME (S.D.M.G, Phantasm, Vortex Beater)
+            else if (id == 1553 || id == 3475 || id == 3476)
+            {
+                projID = 614; // Nebula Sphere
+                count = 3;
+                dmgMult = 1.2f;
+            }
+            // 7. ELEMEN DARAH/CRIMSON (Dart Pistol, Tendon Bow)
+            else if (id == 3020 || id == 796)
+            {
+                projID = 305; // Ichor Splash
+            }
 
-            // Jika masuk kategori, tembak!
+            // Eksekusi penembakan proyektil elemen
             if (projID != -1)
             {
-                int pID = Projectile.NewProjectile(null, pos, vel, projID, (int)(item.damage * damageMult), 5f, tsPlayer.Index);
-                NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, pID);
+                for (int i = 0; i < count; i++)
+                {
+                    float spread = MathHelper.ToRadians(rand.Next(-8, 9));
+                    int pID = Projectile.NewProjectile(null, pos, vel.RotatedBy(spread), projID, (int)(item.damage * dmgMult), 3f, tsPlayer.Index);
+                    NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, pID);
+                }
             }
-            // DEFAULT: Efek acak tipis-tipis buat pedang kayu/besi biasa
+            // DEFAULT: Efek laser Twins standar untuk senjata lainnya
             else
             {
-                int[] common = { 132, 157 }; // Terra beam kecil atau ice sickle
-                int pID = Projectile.NewProjectile(null, pos, vel, common[new Random().Next(2)], (int)(item.damage * 0.5), 2f, tsPlayer.Index);
+                int pID = Projectile.NewProjectile(null, pos, vel, 83, (int)(item.damage * 0.7), 2f, tsPlayer.Index);
                 NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, pID);
             }
         }
