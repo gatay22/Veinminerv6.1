650d0a81a74616e417977409cb7d2e0f5327465e
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 19:32:05 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -1,19 +1,20 @@
 using System;
+using System.IO;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 
-namespace RangedVarietyPlugin
+namespace UniqueMeleeMaster
 {
     [ApiVersion(2, 1)]
-    public class RangedPlugin : TerrariaPlugin
+    public class MeleePlugin : TerrariaPlugin
     {
-        public override string Name => "Ultimate Ranged Variety";
-        public override string Author => "Player";
-        public override Version Version => new Version(2, 1, 0);
+        public override string Name => "Personalized Melee Arsenal";
+        public override string Author => "Gemini";
+        public override Version Version => new Version(2, 4, 0);
 
-        public RangedPlugin(Main game) : base(game) { }
+        public MeleePlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
@@ -30,50 +31,65 @@ namespace RangedVarietyPlugin
             Player p = tsPlayer.TPlayer;
             Item item = p.inventory[p.selectedItem];
 
-            // Filter hanya senjata Ranged
-            if (item != null && item.damage > 0 && item.ranged)
+            if (item != null && item.damage > 0 && item.melee)
             {
                 if (p.itemAnimation == p.itemAnimationMax - 1 && p.itemAnimation > 0)
                 {
-                    SpawnRandomRangedAttack(tsPlayer, item);
+                    ApplyUniqueMeleeEffect(tsPlayer, item);
                 }
             }
         }
 
-        private void SpawnRandomRangedAttack(TSPlayer tsPlayer, Item item)
+        private void ApplyUniqueMeleeEffect(TSPlayer tsPlayer, Item item)
         {
             Vector2 pos = tsPlayer.TPlayer.Center;
             int dir = tsPlayer.TPlayer.direction;
-            Vector2 vel = new Vector2(dir * 14f, 0f); // Kecepatan dasar
-            Random rand = new Random();
+            Vector2 vel = new Vector2(dir * 14f, 0f);
+            int id = item.type;
 
-            // Kumpulan ID Proyektil Keren
-            // 83: Laser Merah, 84: Laser Hijau, 100: Death Laser, 226: Chlorophyte, 631: Daybreak
-            int[] projPool = { 83, 84, 100, 226, 631, 242, 440, 118 };
+            int projID = -1;
+            float damageMult = 1f;
+
+            // 1. KATEGORI API (Volcano, Molten, Solar)
+            if (id == 121 || id == 3473 || id == 112) projID = 15; 
             
-            // Pilih 1 serangan secara acak dari pool
-            int selectedProj = projPool[rand.Next(projPool.Length)];
+            // 2. KATEGORI ES (Ice Blade, Frostbrand)
+            else if (id == 724 || id == 674 || id == 675) projID = 118;
 
-            // Variasi jumlah proyektil (antara 1 sampai 3)
-            int amount = rand.Next(1, 4);
+            // 3. KATEGORI SHADOW/DARK (Night's Edge, Breaker Blade)
+            else if (id == 273 || id == 46) projID = 496; // Shadowflame burst
 
-            for (int i = 0; i < amount; i++)
+            // 4. KATEGORI BLOOD/CRIMSON (Blood Butcherer, Blademaster)
+            else if (id == 795 || id == 3211) projID = 305; // Ichor splash
+
+            // 5. KATEGORI HALLOWED/LIGHT (Excalibur, Gungnir)
+            else if (id == 368 || id == 550) projID = 156; // Beam Sword ray
+
+            // 6. KATEGORI NATURE (Blade of Grass, Muramasa)
+            else if (id == 190 || id == 155) projID = 228; // Spore cloud
+
+            // 7. KATEGORI COSMIC/ENDGAME (Terra Blade, Influx Waver, Star Wrath)
+            else if (id == 757 || id == 2880 || id == 3063) 
             {
-                // Kasih sedikit spread (acak arah) supaya tidak numpuk di satu garis
-                float spread = MathHelper.ToRadians(rand.Next(-15, 16));
-                Vector2 finalVel = vel.RotatedBy(spread);
+                projID = 614; // Nebula sphere
+                damageMult = 1.2f;
+            }
+            
+            // 8. KATEGORI KHUSUS ZENITH (Kalau ada yang punya)
+            else if (id == 4956) projID = 706;
 
-                int pID = Projectile.NewProjectile(null, pos, finalVel, selectedProj, item.damage, 3f, tsPlayer.Index);
-                
-                // Beritahu server untuk kirim proyektil ke semua player
+            // Jika masuk kategori, tembak!
+            if (projID != -1)
+            {
+                int pID = Projectile.NewProjectile(null, pos, vel, projID, (int)(item.damage * damageMult), 5f, tsPlayer.Index);
                 NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, pID);
             }
-
-            // Bonus: 10% peluang muncul ledakan Shadowflame di posisi player saat nembak
-            if (rand.Next(10) == 0)
+            // DEFAULT: Efek acak tipis-tipis buat pedang kayu/besi biasa
+            else
             {
-                int boom = Projectile.NewProjectile(null, pos, Vector2.Zero, 496, item.damage, 5f, tsPlayer.Index);
-                NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, boom);
+                int[] common = { 132, 157 }; // Terra beam kecil atau ice sickle
+                int pID = Projectile.NewProjectile(null, pos, vel, common[new Random().Next(2)], (int)(item.damage * 0.5), 2f, tsPlayer.Index);
+                NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, pID);
             }
         }
 
