c1e1e575c23777299e9b46078d21bf0056f66a8b
gatay22 <tangguljatiroto@gmail.com>
2026-04-10 17:10:09 +0700

    Update VeinMiner.cs

--- VeinMiner.cs
+++ VeinMiner.cs
@@ -4,16 +4,19 @@ using TerrariaApi.Server;
 using TShockAPI;
 using Microsoft.Xna.Framework;
 
-namespace RangedTwinsFinal
+namespace ChestSelfDestruct
 {
     [ApiVersion(2, 1)]
-    public class RangedPlugin : TerrariaPlugin
+    public class RandomLootPlugin : TerrariaPlugin
     {
-        public override string Name => "Twins Laser Ranged Fixed";
+        public override string Name => "Self-Destruct Random Chest";
         public override string Author => "Player";
-        public override Version Version => new Version(1, 2, 2);
+        public override Version Version => new Version(1, 2, 0);
 
-        public RangedPlugin(Main game) : base(game) { }
+        // Pool item: Life Crystal, Bar, Potion, Rare Materials
+        private int[] lootPool = { 29, 21, 19, 706, 31, 2353, 290, 292, 499, 1225 };
+
+        public RandomLootPlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
@@ -22,45 +25,59 @@ namespace RangedTwinsFinal
 
         private void OnGetData(GetDataEventArgs args)
         {
-            if (args.MsgID != PacketTypes.PlayerUpdate) return;
+            // Packet 31: Saat player berinteraksi dengan Chest
+            if (args.MsgID != PacketTypes.ChestOpen) return;
 
             TSPlayer tsPlayer = TShock.Players[args.Msg.whoAmI];
-            if (tsPlayer == null || !tsPlayer.Active || tsPlayer.TPlayer == null) return;
+            if (tsPlayer == null || !tsPlayer.Active) return;
 
-            Player p = tsPlayer.TPlayer;
-            Item item = p.inventory[p.selectedItem];
-
-            if (item != null && item.damage > 0 && item.ranged)
+            using (var reader = new System.IO.BinaryReader(new System.IO.MemoryStream(args.Msg.readBuffer, args.Index, args.Length)))
             {
-                if (p.itemAnimation == p.itemAnimationMax - 1 && p.itemAnimation > 0)
+                short x = reader.ReadInt16();
+                short y = reader.ReadInt16();
+
+                int chestID = Chest.FindChest(x, y);
+                if (chestID != -1)
                 {
-                    ApplyTwinsLaser(tsPlayer, item);
+                    // 1. Acak isi Chest khusus untuk player tersebut
+                    RandomizeChest(chestID);
+
+                    // 2. Efek Visual/Pesan (Self-Destruct Warning)
+                    tsPlayer.SendMessage("PERINGATAN: Chest ini akan meledak/hancur dalam sekejap!", Color.Red);
+
+                    // 3. Hancurkan Tile di Map (Self-Destruct)
+                    // Menggunakan WorldGen.KillTile agar chest hilang dari map tanpa drop item chest-nya
+                    WorldGen.KillTile(x, y, false, false, false);
+
+                    // 4. Sinkronisasi ke semua player bahwa chest tersebut sudah hilang
+                    NetMessage.SendData((int)PacketTypes.TileClean, -1, -1, null, x, y);
+                    
+                    // Tambahan efek suara ledakan di posisi chest (opsional secara visual server)
+                    // Player akan melihat chest hilang dari hadapan mereka setelah dibuka.
                 }
             }
         }
 
-        private void ApplyTwinsLaser(TSPlayer tsPlayer, Item item)
+        private void RandomizeChest(int chestID)
         {
-            if (Main.rand.Next(5) != 0) return;
+            Chest chest = Main.chest[chestID];
+            if (chest == null) return;
 
-            Vector2 pos = tsPlayer.TPlayer.Center;
-            
-            // Karena lastMouseX tidak ada di server, kita gunakan arah hadap player (direction)
-            // 16f adalah kecepatan laser
-            Vector2 baseVelocity = new Vector2(tsPlayer.TPlayer.direction * 16f, 0f);
+            Random rand = new Random();
 
-            int laserProjID = 83; // Laser Merah Twins
-
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 40; i++)
             {
-                // Variasi sudut supaya menyebar (kipas)
-                float spread = MathHelper.ToRadians(-10f + (i * 5f)); 
-                Vector2 shotVelocity = baseVelocity.RotatedBy(spread);
-
-                // Damage murni mengikuti senjata
-                int proj = Projectile.NewProjectile(null, pos, shotVelocity, laserProjID, item.damage, 4f, tsPlayer.Index);
-                
-                NetMessage.SendData((int)PacketTypes.ProjectileNew, -1, -1, null, proj);
+                // Isi 4-8 slot secara acak
+                if (i < rand.Next(4, 9))
+                {
+                    int itemID = lootPool[rand.Next(lootPool.Length)];
+                    chest.item[i].SetDefaults(itemID);
+                    chest.item[i].stack = rand.Next(1, 6);
+                }
+                else
+                {
+                    chest.item[i].SetDefaults(0);
+                }
             }
         }
 
